1db8b3cef81b0571a0cf538df14b0adb0e7b31cb
AFlo59 <etudessup59230@gmail.com>
2024-05-14 16:38:56 +0200

    up

--- database/i_am_lost/model.py
+++ database/i_am_lost/model.py
@@ -11,21 +11,15 @@ Base = declarative_base()
 
 class Account(Base):
     __tablename__ = 'account'
-    id = Column(Integer, primary_key=True)
+    account_id = Column(Integer, primary_key=True)
     balance = Column(Float, default=0.0)
-    transactions = relationship("Transaction", secondary="account_transaction_association", back_populates="accounts")
+    transactions = relationship("Transaction", back_populates="accounts")
 
 class Transaction(Base):
     __tablename__ = 'transaction'
-    id = Column(Integer, primary_key=True)
+    transaction_id = Column(Integer, primary_key=True)
     amount = Column(Float)
     type = Column(String)
     timestamp = Column(DateTime, default=datetime.now)
-    accounts = relationship("Account", secondary="account_transaction_association", back_populates="transactions")
+    account = relationship("Account", back_populates="transactions")
 
-class AccountTransactionAssociation(Base):
-    __tablename__ = 'account_transaction_association'
-    account_id = Column(Integer, ForeignKey('account.id'), primary_key=True)
-    transaction_id = Column(Integer, ForeignKey('transaction.id'), primary_key=True)
-    account = relationship("Account", backref="account_transactions")
-    transaction = relationship("Transaction", backref="transaction_accounts")
