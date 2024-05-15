a88ee981b1dbbcadf97270b003bf3b5084cdb3b1
AFlo59 <etudessup59230@gmail.com>
2024-05-15 13:34:15 +0200

    up

--- tests/tests_custom/ conftest.py
+++ tests/tests_custom/ conftest.py
@@ -3,24 +3,24 @@ from database.Custom.model import *
 
 @pytest.fixture
 def account_factory():
-    def create_account():
-        return Account()
+    def create_account(initial_balance):
+        return Account(balance=initial_balance)
     return create_account
 
 @pytest.fixture
 def deposit_factory():
-    def deposit():
-        return Transaction()
+    def deposit(amount, account):
+        return Transaction(amount=amount, account=account)
     return deposit
 
 @pytest.fixture
 def withdraw_factory():
-    def withdraw():
-        return Transaction()
+    def withdraw(amount, account):
+        return Transaction(amount=amount, account=account)
     return withdraw
 
 @pytest.fixture
 def transfer_factory():
-    def transfer():
-        return Transaction()
+    def transfer( amount, account_source, account_target):
+        return Transaction(amount=amount, account_source=account_source, account_target=account_target)
     return transfer
